7c04605cec6e546340661aebaa7168902c1b423a
Sergejs Hamjonoks <troll.legacy@gmail.com>
2023-01-06 17:21:25 +0000

    added some pseudocode and prompt function

--- script.js
+++ script.js
@@ -87,20 +87,58 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
+const password =[];
+// Pseudocode 
+// Ask about password length
+// Ask about special characters and generate random ammount < passwordLength  and push to array
+// Ask about numeric characters and generate random ammount < passwordLength - amount of special characters generated and push to array
+// Ask about lower case characters generate random ammount < passwordLength - amount of special characters generated - numeric characters generated and push to array
+// Ask about upper case characters generate random ammount < passwordLength - amount of special characters generated - numeric characters generated - amount of lower case generated and push to array
+// Mix password array using Fisher-Yates shuffle algorithm (reference: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
+// Use for/for each to display every character of Array
+
 
 // Function to prompt user for password options
-function getPasswordOptions() {
-
+function getPasswordOptions() { 
+  let passwordLength = prompt ("Please enter desirable password length from 10 to 64");
+    if (passwordLength >= 10 && passwordLength <= 64 ) {
+      password.length = passwordLength;
+      const specialCharactersConfirmation = confirm("Would you like to include special characters?");
+      const numericCharactersConfirmation = confirm ("Would you like to include numeric characters?");
+      const lowerCasedCharactersConfirmation = confirm ("Would you like to include lower cased characters?");
+      const upperCasedCharactersConfirmation = confirm ("Would you like to include upper cased characters?");
+      
+      
+/*       if (specialCharactersConfirmation === true) {
+        getRandom (specialCharacters);
+      }
+      
+      if (numericCharactersConfirmation === true) {
+        getRandom(numericCharacters);
+      }
+      
+      if (lowerCasedCharactersConfirmation === true) {
+        getRandom(lowerCasedCharacters);
+      }
+      
+      if (upperCasedCharactersConfirmation === true) {
+        getRandom(upperCasedCharacters);
+      }  */
+     
+    }
+    else alert("PLease enter length from 10 to 64");
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  if (getPasswordOptions.specialCharactersConfirmation === true) {
+    console.log ("hello");
+  }
 }
 
 // Get references to the #generate element
@@ -113,6 +151,7 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+getPasswordOptions();
+generatePassword();
